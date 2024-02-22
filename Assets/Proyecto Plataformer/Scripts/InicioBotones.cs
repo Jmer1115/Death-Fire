0d93748914ad85c604672ac8a06f562e436bdbc1
Alejandro Lopez <alejolr1218@gmail.com>
2024-02-22 10:45:57 -0500

    Añadido de confirmation box, y finalizando funcion botones

--- Assets/Proyecto Plataformer/Scripts/InicioBotones.cs
+++ Assets/Proyecto Plataformer/Scripts/InicioBotones.cs
@@ -6,6 +6,8 @@ using MoreMountains.Tools;
 
 public class InicioBotones : MonoBehaviour
 {
+    [SerializeField] GameObject quitPanel;
+    
     public void BotonJugar()
     {
         MMSceneLoadingManager.LoadScene("Nivel 1");
@@ -19,5 +21,20 @@ public class InicioBotones : MonoBehaviour
     public void BotonInicio()
     {
         MMSceneLoadingManager.LoadScene("Inicio");
+    }
+
+    public void BotonInstrucciones()
+    {
+        MMSceneLoadingManager.LoadScene("Instrucciones");
+    }
+
+    public void AbrirConfirmacion()
+    {
+        quitPanel.SetActive(true);
+    }
+
+    public void CerrarConfirmacion()
+    {
+        quitPanel.SetActive(false);
     }
 }
