fd98760f3d53debc4e4ad0eaddcb55ebc48ca220
Alejandro <alejolr1218@gmail.com>
2024-04-25 10:15:12 -0500

    Pelea Final terminada

---
+++ Assets/Proyecto Plataformer/Scripts/FinalBossAchievement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+using UnityEngine;
+
+public class FinalBossAchievement : MonoBehaviour
+{
+    [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject ganaste;
+    private Health bossHealth;
+    private bool won = false;
+
+    void Start()
+    {
+        bossHealth = boss.GetComponent<Health>();
+        
+    }
+
+    void Update()
+    {
+        if (bossHealth.CurrentHealth <= 0 && !won){
+            won = true;
+            MMAchievementManager.UnlockAchievement("The END?");
+            LeanTween.moveY(ganaste.GetComponent<RectTransform>(), 0, 0).setDelay(0.5f)
+            .setEase(LeanTweenType.easeInBounce);
+            Invoke("VolverAlMenu", 4f);
+        }
+    }
+
+    public void VolverAlMenu(){
+        MMSceneLoadingManager.LoadScene("Inicio");
+    }
+}
